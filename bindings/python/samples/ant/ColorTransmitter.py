531e0c3e8088ffaf2e5b3713b7877abe5aa0fff0
Josh Jones <jonesjo7@oregonstate.edu>
2020-09-25 16:15:59 -0700

    encoding issue fix

--- bindings/python/samples/ant/ColorTransmitter.py
+++ bindings/python/samples/ant/ColorTransmitter.py
@@ -6,23 +6,6 @@ class ColorTransmitter:
         raise NotImplementedError
 
 
-class ScreenColorTransmitter(ColorTransmitter):
-    def transmit_colors(self, board):
-        print("╔═", end = "")
-        for col in board[0]:
-            print("═══", end = "")
-        print("═╗")
-        for row in board:
-            print("║ ", end="")
-            for col in row:
-                print(f"\033[{col}m   ", end="\033[m")
-            print(" ║")
-        print("╚═", end="")
-        for col in board[0]:
-            print("═══", end="")
-        print("═╝")
-
-
 class LEDColorTransmitter(ColorTransmitter):
     def transmit_colors(self, board):
         pass
